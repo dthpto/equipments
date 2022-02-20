4e5b2022666856b5eb09bd4a88d601b1231c60af
Alexander Musikhin <dth.pto@gmail.com>
2022-02-20 20:57:35 +0300

    Created routes, controller with blank functions

---
+++ app/Http/Controllers/EquipmentsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EquipmentsController extends Controller
+{
+    public function getEquipments($id = null){
+        echo 'get eq ' . $id;
+    }
+
+    public function createEquipment(){
+        echo 'create eq';
+    }
+
+    public function updateEquipment($id = null){
+        echo 'update eq ' . $id;
+    }
+
+    public function deleteEquipment($id = null){
+        echo 'delete eq ' . $id;
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipmentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix('/equipment')->group(function () {
+    Route::get('/', [EquipmentsController::class, 'getEquipments']);
+    Route::get('/{id}', [EquipmentsController::class, 'getEquipments']);
+    Route::post('/', [EquipmentsController::class, 'createEquipment']);
+    Route::put('/{id}', [EquipmentsController::class, 'updateEquipment'])->where(['id' => '0-9+']);
+    Route::delete('/{id}', [EquipmentsController::class, 'deleteEquipment'])->where(['id' => '0-9+']);
 });
+
