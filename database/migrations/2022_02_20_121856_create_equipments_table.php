2c625a53764707054d1c996cb024d42797b52633
Alexander Musikhin <dth.pto@gmail.com>
2022-02-20 18:08:08 +0500

    Created models and migrations

---
+++ database/migrations/2022_02_20_121856_create_equipments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('equipment_type_id')->unsigned()->default(0);
+            $table->foreign('equipment_type_id')->references('id')->on('equipment_types');
+            $table->string('serial_number')->default('');
+            $table->text('comment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+}
